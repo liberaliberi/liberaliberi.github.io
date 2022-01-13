d17c5a8d30eb37a5c495f337eb6c2939f8fa5db8
liberaliberi <duruwa815@gmail.com>
2022-01-13 16:52:15 +0900

    upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  ...process.env.NODE_ENV ==="production" && {
+    images: {
+      loader: "imgix",
+      path: "https://liberaliberi.github.io/", // 수정 필요
+    },
+  },
+  
 }
 
 module.exports = nextConfig
