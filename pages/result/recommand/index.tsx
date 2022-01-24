539d14e69cdb6cfddd82e724f7b314ae94902f4a
jfmam <jfmam@naver.com>
2022-01-24 12:14:51 +0900

    fix build error

--- pages/result/recommand/index.tsx
+++ pages/result/recommand/index.tsx
@@ -1,5 +1,4 @@
 import styles from "../../../styles/result.module.scss";
-import { H2 } from "../components/header";
 
 interface RecommandProps {
   imgName: string;
@@ -9,7 +8,7 @@ interface RecommandProps {
 export default function Recommand({ imgName, imgUrl }: RecommandProps) {
   return (
     <>
-      <H2>이런 당신을 취향 저격할 장소를 추천해드려요!</H2>
+      <h2>이런 당신을 취향 저격할 장소를 추천해드려요!</h2>
       <figure>
         <figcaption className={styles.imgTitle}>{imgName}</figcaption>
         {/* img 태그 추가적으로 넣기 */}
