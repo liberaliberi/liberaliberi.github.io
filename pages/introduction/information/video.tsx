e9fe4bea2681d31f12a602ec80867db4036a9d34
jfmam <jfmam@naver.com>
2022-01-28 23:41:34 +0900

    제발 되게해주세요

--- pages/introduction/information/video.tsx
+++ pages/introduction/information/video.tsx
@@ -1,17 +1,10 @@
 export default function Video() {
   return (
     <>
-      <video
-        src="/introduction.mp4"
-        controls
-        autoPlay
-        muted
-        playsInline
-        loop
-      >
-        {/* <source src="/introduction.webm" type="video/webm" />
-        <source src="/introduction.mp4" type="video/mp4" />
-        비디오를 지원하지 않습니다. */}
+      <video preload="metadata" controls={true} autoPlay muted playsInline loop>
+        <source src="introduce.webm" type="video/webm" />
+        <source src="introduce.mp4" type="video/mp4" />
+        비디오를 지원하지 않습니다.
       </video>
     </>
   );
