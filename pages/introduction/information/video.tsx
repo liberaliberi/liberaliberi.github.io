73e16e06d041b72a8965ead7c096a5f26bbd2cd5
jfmam <jfmam@naver.com>
2022-01-28 23:57:22 +0900

    alternative video to youtube

--- pages/introduction/information/video.tsx
+++ pages/introduction/information/video.tsx
@@ -1,11 +1,17 @@
+import styles from "../../../public/introduction.module.scss";
+
 export default function Video() {
   return (
     <>
-      <video preload="metadata" controls={true} autoPlay muted playsInline loop>
-        <source src="introduce.webm" type="video/webm" />
-        <source src="introduce.mp4" type="video/mp4" />
-        비디오를 지원하지 않습니다.
-      </video>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/cBS7d1RY4vg"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </>
   );
 }
