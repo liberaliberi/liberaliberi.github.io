9c2cc2cdec1cbf0a50a8f515d6130aa49f9dc45c
jfmam <jfmam@naver.com>
2022-01-28 18:35:45 +0900

    update video tag

--- pages/introduction/information/video.tsx
+++ pages/introduction/information/video.tsx
@@ -1,16 +1,10 @@
 export default function Video() {
   return (
     <>
-      <video
-        controls
-        autoPlay
-        muted
-        playsInline
-        loop
-      >
+      <video controls autoPlay muted playsInline loop>
         <source src="/introduction.webm" type="video/webm" />
         <source src="/introduction.mp4" type="video/mp4" />
-        <div>비디오를 지원하지 않습니다.</div>
+        비디오를 지원하지 않습니다.
       </video>
     </>
   );
